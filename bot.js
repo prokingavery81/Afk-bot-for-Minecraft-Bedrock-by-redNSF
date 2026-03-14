395614caa69f40ee8d7e29d4b5fbfe296eb453a8
prokingavery81 <Boxedbyavery03@gmail.com>
2026-03-14 16:34:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,4 +4,5 @@ const client = bedrock.createClient({
   port: 19132,         // optional, default 19132
   username: 'Notch',   // the username you want to join as, optional if online mode
   offline: true       // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
+"start"
 })
