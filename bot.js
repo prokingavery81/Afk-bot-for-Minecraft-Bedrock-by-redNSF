22f6ab398be8afb7b1f751509a49ddbc3476d04f
prokingavery81 <Boxedbyavery03@gmail.com>
2026-03-14 16:38:16 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const bedrock = require('bedrock-protocol')
 const client = bedrock.createClient({
-  host: 'server_ip',   
-  port: 19132,         // optional, default 19132
-  username: 'Notch',   // the username you want to join as, optional if online mode
-  offline: true       // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
+  host: 'ShadowSMP-H0ul.aternos.me',   
+  port: 35404,         // optional, default 19132
+  username: 'ShadowBot',   // the username you want to join as, optional if online mode
+  offline: false       // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
+"start"
 })
